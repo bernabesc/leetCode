9a3b2b12c72565c95de5abcb69e6185411b63416
Bernabé Soto <37323479+bernabesc@users.noreply.github.com>
2021-08-10 20:22:09 -0700

    Time: 11 ms (6.70%), Space: 37.4 MB (14.23%) - LeetHub

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -1,18 +1,17 @@
 class Solution {
     public int reverse(int x) {
-        int units = ((Integer)Math.abs(x)).toString().length();
         String reverseIntTxt = "";
-        if(x<0)
-            reverseIntTxt="-";
-        for(int a=0;a<units;a++,x/=10){
+        if(x<0) {
+            reverseIntTxt = "-";
+        }
+        while(x!=0){
             reverseIntTxt+=Math.abs(x%10);
+            x/=10;
         }
         try{
             return Integer.parseInt(reverseIntTxt);
         } catch (Exception NumberFormatException){
             return 0;
         }
-        
-        
     }
 }
